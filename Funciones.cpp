ea2be447d324d6fe7c5a717a4316769211b7553a
Oswal <rene12215@gmail.com>
2017-04-27 09:15:13 -0600

    Funcion factorial

--- Funciones.cpp
+++ Funciones.cpp
@@ -6,11 +6,13 @@ La funcion muestras el menu y valida la entrada que sea
  correcta y retorna la opcion elegida por el usuario
 */
 int menu();
+//Funcion para calcular factorial
+int factorial(int num);
 
 int main(){
 
   bool salir=false;
-  while(!salir){    
+  while(!salir){
     switch(menu()){
       case 1:{
         int num1,num2;
@@ -32,7 +34,10 @@ int main(){
        break;
      }
      case 3:{
-
+       int num5;
+       cout<<"Ingrese un numero para calcular factorial: ";
+       cin>>num5;
+       cout<<"El factorial de "<< num5 <<" es: "<< factorial(num5)<<endl;
        break;
      }
      case 4:
@@ -49,7 +54,7 @@ int menu(){
   int opcion;
   bool valido=false;
   do{
- 
+
     cout<<"-----MENU-----"<<endl
     <<"1.- Multiplicar dos numeros"<<endl
     <<"2.- Sumar dos numeros"<<endl
@@ -70,3 +75,8 @@ int menu(){
   return opcion;
 }
 
+int factorial(int num){
+  if(num<0) return 0;
+  else if(num>1)return num*factorial(num-1);
+  return 1;
+}
