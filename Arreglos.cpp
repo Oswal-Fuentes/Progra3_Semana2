5ffd7006a5f0513d5806b9a140c2e91fd622108a
Oswal <rene12215@gmail.com>
2017-05-01 12:06:25 -0600

    Arreglos

---
+++ Arreglos.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int size=5;
+  int arreglo[size];//Lo mas recomendado si puede variar el tamaño del arreglo
+
+  int arreglo2[]={10,20,30,40,50};
+  char diasSemana[]={'L','M','m','J','V','S','D'};
+
+  //Matrices
+  int matriz[5][5];
+
+  //new delete - libera memoria
+
+  return 0;
+}
